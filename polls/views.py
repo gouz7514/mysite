b3a0b99c1d9fc92589d95e73af858c441edebeb5
gouz7514 <gouz7514@gmail.com>
2021-12-21 23:23:03 +0900

    Update : Http404 for 404 view, template

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
+from django.http.response import Http404
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  # polls/index.html 템플릿을 불러온 후, context를 전달
-  template = loader.get_template('polls/index.html')
-  context = {
-    'latest_question_list': latest_question_list,
-  }
-  return HttpResponse(template.render(context, request))
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = "You're looking at the resuls of question %s."
